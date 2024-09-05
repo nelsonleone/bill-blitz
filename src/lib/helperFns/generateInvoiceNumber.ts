a139f85cc6af1d7891176dd674bb8011dc64ed46
Onuegbu Nelson <95982650+nelsonleone@users.noreply.github.com>
2024-09-05 20:59:07 +0100

    Update generateInvoiceNumber.ts

--- src/lib/helperFns/generateInvoiceNumber.ts
+++ src/lib/helperFns/generateInvoiceNumber.ts
@@ -2,6 +2,13 @@ export function generateInvoiceNumber(date: Date = new Date()): string {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0')
     const day = String(date.getDate()).padStart(2, '0')
-    return `IN${year}${month}${day}`;
+
+    const numArr = []
+
+    while (numArr.length < 3){
+        numArr.push(Math.floor(Math.random() * 9))
+    }
+
+    return `IN${year}${month}${day}-${numArr}`;
 }
   
