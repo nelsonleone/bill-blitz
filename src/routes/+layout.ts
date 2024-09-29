5f26c80b48bb2406eb42a5bc49372637f2382084
Onuegbu Nelson <95982650+nelsonleone@users.noreply.github.com>
2024-09-29 08:12:08 +0100

    added prerender

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,8 @@
 import { createBrowserClient, createServerClient, isBrowser, parse } from '@supabase/ssr'
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
 
+export const prerender = true;
+
 import type { LayoutLoad } from './$types'
 
 export const load: LayoutLoad = async ({ data, depends, fetch }) => {
